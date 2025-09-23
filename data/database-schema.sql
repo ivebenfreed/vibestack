7f2864d400418ac248fcb6572670164fdff01000
Ben Freed <ben@getelevra.com>
2025-09-23 17:37:41 -0400

    [TEMP] Session snapshot - 2025-09-23 17:37:41

--- data/database-schema.sql
+++ data/database-schema.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database cluster dump
 --
 
-\restrict BsUuwu61MKO0rqDmcaIULkFCq4vzRxhUYFBopHwpQQ6Db8mhnt2MiRV4lBNiqtE
+\restrict JjhErCmCYtW3vXIURqnGF0hbKhEgYgWbdm67awbaUUSYthLWpYXlrGprn3uQLkX
 
 SET default_transaction_read_only = off;
 
@@ -35,7 +35,7 @@ ALTER ROLE vibestack_app_user WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB L
 
 
 
-\unrestrict BsUuwu61MKO0rqDmcaIULkFCq4vzRxhUYFBopHwpQQ6Db8mhnt2MiRV4lBNiqtE
+\unrestrict JjhErCmCYtW3vXIURqnGF0hbKhEgYgWbdm67awbaUUSYthLWpYXlrGprn3uQLkX
 
 --
 -- Databases
@@ -51,7 +51,7 @@ ALTER ROLE vibestack_app_user WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB L
 -- PostgreSQL database dump
 --
 
-\restrict KABhD6EY9gppSTkEzPjgvK9ieK5JTsnt76sREuSfAEeqOkpLgKJURXkWNoLVVj4
+\restrict sPTxHSjPINEGducTmWawISo82yYFcW5AOCN0aK7N4thvymYTztwJXNpBxXffaw6
 
 -- Dumped from database version 17.6 (Debian 17.6-1.pgdg12+1)
 -- Dumped by pg_dump version 17.6 (Debian 17.6-1.pgdg12+1)
@@ -72,7 +72,7 @@ SET row_security = off;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict KABhD6EY9gppSTkEzPjgvK9ieK5JTsnt76sREuSfAEeqOkpLgKJURXkWNoLVVj4
+\unrestrict sPTxHSjPINEGducTmWawISo82yYFcW5AOCN0aK7N4thvymYTztwJXNpBxXffaw6
 
 --
 -- Database "elevra_dev" dump
@@ -82,7 +82,7 @@ SET row_security = off;
 -- PostgreSQL database dump
 --
 
-\restrict 1mS10bbD0vGd0GKl1c1fZ9aYQqbmoSFCRCTO2mRZiOCeIm1HWXGaw7d00irTxjo
+\restrict 525KtfEuZyeaIqiYJaZUqAY2BIY35JplzbCCNs88fxgonrcYtEu1vV3yupaXhSt
 
 -- Dumped from database version 17.6 (Debian 17.6-1.pgdg12+1)
 -- Dumped by pg_dump version 17.6 (Debian 17.6-1.pgdg12+1)
@@ -108,9 +108,9 @@ CREATE DATABASE elevra_dev WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PR
 
 ALTER DATABASE elevra_dev OWNER TO postgres;
 
-\unrestrict 1mS10bbD0vGd0GKl1c1fZ9aYQqbmoSFCRCTO2mRZiOCeIm1HWXGaw7d00irTxjo
+\unrestrict 525KtfEuZyeaIqiYJaZUqAY2BIY35JplzbCCNs88fxgonrcYtEu1vV3yupaXhSt
 \connect elevra_dev
-\restrict 1mS10bbD0vGd0GKl1c1fZ9aYQqbmoSFCRCTO2mRZiOCeIm1HWXGaw7d00irTxjo
+\restrict 525KtfEuZyeaIqiYJaZUqAY2BIY35JplzbCCNs88fxgonrcYtEu1vV3yupaXhSt
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -5351,7 +5351,7 @@ GRANT SELECT ON TABLE public.verification TO test_user;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 1mS10bbD0vGd0GKl1c1fZ9aYQqbmoSFCRCTO2mRZiOCeIm1HWXGaw7d00irTxjo
+\unrestrict 525KtfEuZyeaIqiYJaZUqAY2BIY35JplzbCCNs88fxgonrcYtEu1vV3yupaXhSt
 
 --
 -- Database "postgres" dump
@@ -5363,7 +5363,7 @@ GRANT SELECT ON TABLE public.verification TO test_user;
 -- PostgreSQL database dump
 --
 
-\restrict 4g9LzKUrgMGNhWSrFXxbY6xMfEkTgiyv8NEp1QAGC1oV64Lb71LuZuqCie7fX8B
+\restrict Q6lUxAfEdn8A3e1b8UsPIgDwVy11lhbgYSHaa4WVfPNJUdkLqY1xaA02kEAvtZL
 
 -- Dumped from database version 17.6 (Debian 17.6-1.pgdg12+1)
 -- Dumped by pg_dump version 17.6 (Debian 17.6-1.pgdg12+1)
@@ -5384,7 +5384,7 @@ SET row_security = off;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 4g9LzKUrgMGNhWSrFXxbY6xMfEkTgiyv8NEp1QAGC1oV64Lb71LuZuqCie7fX8B
+\unrestrict Q6lUxAfEdn8A3e1b8UsPIgDwVy11lhbgYSHaa4WVfPNJUdkLqY1xaA02kEAvtZL
 
 --
 -- PostgreSQL database cluster dump complete
--- data/database-seed.sql
+++ data/database-seed.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict XpjOu90TunpYgWk6PE7hALPlb9OoJQzeXV39Sdh3cAfTm5aRBMWWqlm3m9ZvnPN
+\restrict 6qwS5uFekluYb7oUKcAGkQNeiYUH9CKbGhqyMWI9LqJHwpYNjs63hofb0LZ08Gv
 
 -- Dumped from database version 17.6 (Debian 17.6-1.pgdg12+1)
 -- Dumped by pg_dump version 17.6 (Debian 17.6-1.pgdg12+1)
@@ -1314,5 +1314,5 @@ SELECT pg_catalog.setval('public.dataforge_computed_fields_id_seq', 3, true);
 -- PostgreSQL database dump complete
 --
 
-\unrestrict XpjOu90TunpYgWk6PE7hALPlb9OoJQzeXV39Sdh3cAfTm5aRBMWWqlm3m9ZvnPN
+\unrestrict 6qwS5uFekluYb7oUKcAGkQNeiYUH9CKbGhqyMWI9LqJHwpYNjs63hofb0LZ08Gv
 
